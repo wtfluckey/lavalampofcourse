ba3a41051604dfb157c3404a38dc3a90c7cbb8bc
Aly Fluckey <aly@neo.com>
2013-11-04 16:20:05 -0500

    static pages controller - routes for home, about and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Blog::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/about"
+  get "static_pages/contact"
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
